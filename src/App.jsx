dd04eb36694e14323ed2f5f52196c9ea632b35ab
Akash Datta <akash2003datta@gmail.com>
2026-08-09 18:53:38 +0530

    Enhance intro animation handling in App component
    Added functionality to handle portfolio intro animation and updated rendering logic for intro animations.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+
 import React from "react";
+
 import CustomCursor from "./components/CustomCursor";
 import Navbar from "./components/Navbar";
 import ParticlesBackground from "./components/ParticlesBackground";
@@ -12,22 +14,63 @@ import Skills from "./sections/Skills";
 import IntroAnimation from "./components/IntroAnimation";
 
 function App() {
-  const [introDone, setIntroDone]= React.useState(false);
-  return (<>
-  {!introDone && <IntroAnimation onFinish={()=> setIntroDone(true)}/>}
-    {introDone && (
-  <div className="relative gradient text-white">
-    <CustomCursor/>
-    <Navbar />
-    <Home />
-    <About />
-    <Skills />
-    <Projects />
-    <Experience />
-    <Contact />
-    <Footer />
-  </div>
-    )}
-  </>)
+  const [introDone, setIntroDone] = React.useState(false);
+
+  // Controls the intro animation when clicking
+  // "View Project" on My Portfolio
+  const [showPortfolioIntro, setShowPortfolioIntro] =
+    React.useState(false);
+
+  const handlePortfolioClick = () => {
+    setShowPortfolioIntro(true);
+  };
+
+  const handleIntroFinish = () => {
+    setShowPortfolioIntro(false);
+  };
+
+  return (
+    <>
+      {/* Initial intro animation */}
+      {!introDone && (
+        <IntroAnimation
+          onFinish={() => setIntroDone(true)}
+        />
+      )}
+
+      {/* Intro animation when My Portfolio is clicked */}
+      {showPortfolioIntro && (
+        <IntroAnimation
+          onFinish={handleIntroFinish}
+        />
+      )}
+
+      {introDone && (
+        <div className="relative gradient text-white">
+          <CustomCursor />
+
+          <Navbar />
+
+          <Home />
+
+          <About />
+
+          <Skills />
+
+          <Projects
+            onPortfolioClick={handlePortfolioClick}
+          />
+
+          <Experience />
+
+          <Contact />
+
+          <Footer />
+        </div>
+      )}
+    </>
+  );
 }
+
 export default App;
+
